ec9ea393456510ce51f4cb42212afcdda5799530
Alexandre BARAT <alex.barat@gmail.com>
2019-11-26 16:10:47 +0100

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_tips
+  has_many :inscriptions
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :avatar, presence: true
+  validates :city, presence: true
 end
