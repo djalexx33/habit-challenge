5ee732bb1a5d19f1068ba6c2f950697b0e7f8f4d
Alexandre BARAT <alex.barat@gmail.com>
2019-11-27 18:37:16 +0100

    Controllers actions -no Daily surveys create & new

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -5,5 +5,6 @@ class ChallengesController < ApplicationController
 
   def show
     @challenge = Challenge.find(params[:id])
+    @tips = @challenge.tips
   end
 end
--- app/controllers/inscriptions_controller.rb
+++ app/controllers/inscriptions_controller.rb
@@ -1,7 +1,15 @@
 class InscriptionsController < ApplicationController
   def create
+    @user = current_user
+    @challenge = Challenge.find(params[:challenge_id])
+    @inscription = Inscription.new(user: @user, challenge: @challenge)
+    @inscription.save
+    redirect_to user_path(current_user)
   end
 
   def destroy
+    @inscription = Inscription.find(params[:id])
+    @inscription.destroy
+    redirect_to challenges_path
   end
 end
