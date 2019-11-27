ecc25b448ff60f0314b085972a927251b3b49a86
Alexandre BARAT <alex.barat@gmail.com>
2019-11-27 14:00:18 +0100

    controllers on fire

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,10 @@ User.destroy_all
 User.create(first_name: "Leonardo", last_name: "Di Caprio", username: "Leo", email: "leo@gmail.com", avatar: "https://gentlemanmoderne.com/wp-content/uploads/2017/09/leonardo_dicaprio-portrait-blog-homme.jpg
 ", password: "azerty", city: "Los Angeles")
 
+puts "Creating our first challenge"
+Challenge.destroy_all
+
+Challenge.create(title: "Plastic Detox", description: "Reduce your plastic consumption", date: Time.now, photo: "https://images.unsplash.com/photo-1544550581-1bcabf842b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80", badge: "https://cdn4.iconfinder.com/data/icons/no-plastic-flat-save-the-earth/512/no_plastic_bottle-512.png")
+
 puts "Leonardo created"
+puts "Plastic detox challenge created"
