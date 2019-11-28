2aff29b01f0e9d1ea855dee74c82a8b1765ceaa5
Arthur Menard <arthurmenard12@gmail.com>
2019-11-28 18:53:38 +0100

    Mortal seed done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @user = current_user
+    @questions = Question.all
     @users = User.geocoded #returns Users with coordinates
     @markers = @users.map do |user|
       {
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@
 
 puts "Creating our first user..."
 User.destroy_all
-User.create(first_name: "Leonardo", last_name: "Di Caprio", username: "Leo", email: "leo@gmail.com", avatar: "https://gentlemanmoderne.com/wp-content/uploads/2017/09/leonardo_dicaprio-portrait-blog-homme.jpg", password: "azerty", city: "Los Angeles")
+leo = User.create(first_name: "Leonardo", last_name: "Di Caprio", username: "Leo", email: "leo@gmail.com", avatar: "https://gentlemanmoderne.com/wp-content/uploads/2017/09/leonardo_dicaprio-portrait-blog-homme.jpg", password: "azerty", city: "Los Angeles")
 puts " ----> Leo has been created."
 
 puts "Creating our challenges..."
 Challenge.destroy_all
-Challenge.create(title: "Plastic Free", description: "Ban sigle-use plastic for the month of January, reduce your consumption of other plastics", date: "2019-01-06 19:08:40.011480", photo: "https://i.pinimg.com/736x/5e/c7/8d/5ec78d8a78867aacb099a7956e834753.jpg", badge: "https://toppng.com/public/uploads/preview/free-png-plastic-water-bottle-png-11519803083hnhiljypmg.png")
+plastic = Challenge.create(title: "Plastic Free", description: "Ban sigle-use plastic for the month of January, reduce your consumption of other plastics", date: "2019-01-06 19:08:40.011480", photo: "https://i.pinimg.com/736x/5e/c7/8d/5ec78d8a78867aacb099a7956e834753.jpg", badge: "https://toppng.com/public/uploads/preview/free-png-plastic-water-bottle-png-11519803083hnhiljypmg.png")
 Challenge.create(title: "Meatless Detox", description: "Stop eating meat completely for the month of February, reduce your consumption of other animal products such as dairy and eggs", date: "2019-02-06 19:08:40.011480", photo: "https://www.wilderutopia.com/wp-content/uploads/2014/10/Beef-Cattle-Factory-Farm-Socially-Responsible-Agriculture-Flickr.jpg", badge: "https://images.emojiterra.com/twitter/v12/512px/1f969.png")
 Challenge.create(title: "Seasocal Fever", description: "Eat only local and seasonal, consume products that grow at this season and that did not travel from far away. ", date: "2019-03-06 19:08:40.011480", photo: "https://thenypost.files.wordpress.com/2019/05/crop-farmer.jpg?quality=90&strip=all&w=618&h=410&crop=1", badge: "https://www.teetransfers.co.uk/images/products/tools_0016_Layer%2012.jpg")
 Challenge.create(title: "Petrol Cure", description: "Reduce your petrol consumption, take the bike, walk, use public transportation", date: "2019-04-06 19:08:40.011480", photo: "https://www.aydinlik.com.tr/_usr/files/img/2017111709322019_i1hp7qcm011mbu2767gdm3rnf11.gif", badge: "http://pngimg.com/uploads/fuel/fuel_PNG49.png")
@@ -26,3 +26,32 @@ Challenge.create(title: "Green Energy Switch", description: "Support green energ
 Challenge.create(title: "Get Involved", description: "Get involved, join an association, start devoting time for the protection of the environment", date: "2019-11-06 19:08:40.011480", photo: "https://vosizneias.com/wp-content/uploads/2018/10/AP18286417971595.jpg", badge: "https://fitnessactivist.com/wp-content/uploads/2016/09/Fitness-activist-header-logo.jpg")
 puts " ----> 11 challenges have been created"
 
+puts "Creating inscription..."
+Inscription.create(challenge: plastic, user: leo)
+
+puts "Creating questions..."
+
+question_1 = Question.create(title: "Did you use platic today", challenge: plastic)
+question_2 = Question.create(title: "Did you eat plastic today", challenge: plastic)
+question_3 = Question.create(title: "Did you make plastic today", challenge: plastic)
+
+puts "Creating our options..."
+
+option_1_q1 = Option.create(title: "Between 10 and 15 times", pounderation: 0, question: question_1)
+option_2_q1 = Option.create(title: "Between 0 and 10 times", pounderation: 2, question: question_1)
+option_3_q1 = Option.create(title: "0 time", pounderation: 4, question: question_1)
+
+option_1_q2 = Option.create(title: "Between 10 and 15 times", pounderation: 0, question: question_2)
+option_2_q2 = Option.create(title: "Between 0 and 10 times", pounderation: 2, question: question_2)
+option_3_q2 = Option.create(title: "O time", pounderation: 4, question: question_2)
+
+
+option_1_q3 = Option.create(title: "Between 10 and 15 times", pounderation: 0, question: question_3)
+option_2_q3 = Option.create(title: "Between 0 and 10 times", pounderation: 2, question: question_3)
+option_3_q3 = Option.create(title: "0 time", pounderation: 4, question: question_3)
+
+puts "Creating our survey..."
+
+survey_1 = DailySurvey.create(date: Date.today, inscription: leo.inscriptions.first)
+
+puts "Finished"
