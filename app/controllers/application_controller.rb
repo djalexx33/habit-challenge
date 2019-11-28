ed6991359aa595cd9b86ab67c4224157de8ad34c
Alexandre BARAT <alex.barat@gmail.com>
2019-11-28 15:01:52 +0100

    Navbar + Challenge of the Month fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :set_challenge_of_the_month
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
     fields = [:first_name, :last_name, :username, :city]
     devise_parameter_sanitizer.permit(:sign_up, keys: fields)
     devise_parameter_sanitizer.permit(:account_update, keys: fields)
+  end
+
+  def set_challenge_of_the_month
+    @challenge_of_the_month = Challenge.where('extract(month from date) = ?', Date.today.month).first
   end
 end
