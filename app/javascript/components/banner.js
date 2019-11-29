768f732148fa39061978b1559ab1285d12b1fd7e
Arthur Menard <arthurmenard12@gmail.com>
2019-11-29 12:30:50 +0100

    Banner for each challenge ok

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -8,4 +8,14 @@ const loadDynamicBannerText = () => {
   });
 }
 
+const loadDynamicShowText = () => {
+  new Typed('#how', {
+    strings: ["#How"],
+    typeSpeed: 50,
+    loop: false
+  });
+}
+
 export { loadDynamicBannerText };
+export { loadDynamicShowText };
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,9 @@ import { expand } from '../components/card';
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import { loadDynamicBannerText } from '../components/banner';
+import { loadDynamicShowText } from '../components/banner';
 loadDynamicBannerText();
+loadDynamicShowText();
 
 expand();
 
