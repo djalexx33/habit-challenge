866bcfb889d0e8e8e802329a8f251ada1f1ba6ff
Arthur Menard <arthurmenard12@gmail.com>
2019-12-02 18:51:17 +0100

    Daily surveys and daily answner ok, unique but redirection link on surveys cards doesn't works

--- app/controllers/daily_surveys_controller.rb
+++ app/controllers/daily_surveys_controller.rb
@@ -1,7 +1,33 @@
 class DailySurveysController < ApplicationController
-  def create
+  def new
+    @inscription = Inscription.find(params[:inscription_id])
+    @challenge = @inscription.challenge
+    @questions = @challenge.questions
+    @daily_survey = DailySurvey.new
   end
 
-  def new
+  def create
+    @inscription = Inscription.find(params[:inscription_id])
+    @challenge = @inscription.challenge
+    @questions = @challenge.questions
+    @daily_survey = DailySurvey.new(daily_survey_params)
+    @daily_survey.inscription = @inscription
+    @daily_survey.date = Date.today
+
+    if @daily_survey.save
+      # @daily_survey.score
+      flash[:notice] = 'Successfully created daily survey.'
+
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "You already did this survey for #{Date.today.strftime("%D")}"
+      render :new
+    end
+  end
+
+  private
+
+  def daily_survey_params
+    params.require(:daily_survey).permit(:title, daily_answers_attributes: [:option_id])
   end
 end
