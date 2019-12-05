3d38b592b05b4fdd379bcc3cf7c5a725a482861f
Arthur Menard <arthurmenard12@gmail.com>
2019-12-05 10:24:58 +0100

    better

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -3,5 +3,5 @@ class Tip < ApplicationRecord
   has_many :user_tips, dependent: :destroy
   has_many :users, through: :user_tips, dependent: :destroy
   validates :title, presence: true, uniqueness: true
-  validates :description, presence: true, uniqueness: true
+  validates :description, presence: true
 end
