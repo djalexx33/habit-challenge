0902ebfd8525b90977797ba390ed9b390ea973d3
Alexandre BARAT <alex.barat@gmail.com>
2019-12-05 11:28:14 +0100

    uniqueness removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :challenges, through: :inscriptions
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true
   # validates :avatar, presence: true
   validates :city, presence: true
   mount_uploader :avatar, PhotoUploader
